f09bebf0b5980ec76f63f1019773de97a03765af
smile <s930175@gmail.com>
2022-05-25 23:23:07 +0800

    try

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:"/homework/"
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/homework/'
+  : '/'
 })
